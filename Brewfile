b05e301ca15118e926bdfe73ca1b56ce50f2eb04
nematatu <nematatu1003@gmail.com>
2025-07-09 00:20:42 +0900

    add: setup.sh!

---
+++ Brewfile
@@ -0,0 +1,39 @@
+tap "homebrew/bundle"
+
+# brew
+brew "git"
+brew "neovim"
+brew "mise"
+brew "bat"
+brew "starship"
+brew "eza"
+brew "fd"
+brew "ffmpeg"
+brew "fzf"
+brew "gh"
+brew "ghq"
+brew "lazygit"
+brew "lua-language-server"
+brew "ripgrep"
+brew "tree"
+
+# cask
+cask 'google-chrome'
+cask 'google-japanese-ime'
+cask 'iterm2'
+cask "raycast"
+cask "slack"
+cask "discord"
+cask "karabiner-elements"
+cask "font-hack-nerd-font"
+cask "visual-studio-code"
+cask "shottr"
+cask "cap"
+
+# mas
+mas "TweetShot", id: 1227057295
+mas "RunCat", id: 1429033973
+mas "GIPHY CAPTURE", id: 668208984
+mas "LINE", id: 539883307
+mas "Spark", id: 1176895642
+mas "Fuwari", id: 1187652334
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -uexo pipefail
+
+PATH="$PATH:/opt/homebrew/bin"
+
+# HomeBrewのインストール
+if [ "$(uname)" = "Darwin" ]; then
+    if ! type brew &> /dev/null; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+    else 
+        echo "HomeBrew is already installed."
+    fi
+else
+    echo "Not macOS!"
+fi
+
+/opt/homebrew/bin/brew bundle install --file ./Brewfile
+
+if ! type xcode-select &> /dev/null; then
+    echo "start install Xcode CLT"
+    xcode-select --install
+else
+    echo "Xcode CLT is already installed."
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+for dir in "${SCRIPT_DIR}"/*/; do
+    [[ "$dir" == "${SCRIPT_DIR}/.git/" ]] && continue
+    [[ "$dir" == "${SCRIPT_DIR}"/.github/ ]] && continue
+
+    for dotfile in "${dir}".??* ; do
+        if [[ -f "$dotfile" ]]; then
+            TARGET_PATH="$HOME/$(basename "$dotfile"")"
+            if [[ -e "$TARGET_PATH" && ! -L "$TARGET_PATH" ]]; then
+                echo "Backing up extisting file: $TARGET_PATH -> ${TARGET_PATH}.backup.${TIMESTAMP}"
+                mv "$TARGET_PATH" "${TARGET_PATH}.backup.${TIMESTAMP}"
+            fi
+            ln -fnsv "$dotfile" "$TARGET_PATH"
+        fi
+    done
+done 
+
+ln -fnsv "${SCRIPT_DIR}/mise/mise.toml" "${HOME}/mise.toml"
+if[[ "$(which mise)" != "" ]]; then
+    mise install
+else
+    "echo install the mise command"
+fi
+ 
+if ! which node; then
+    $(/opt/homebrew/bin/brew --prefix)/bin/mise use --global node
+fi
+
+echo "finish setup!👍"
